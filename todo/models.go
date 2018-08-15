6450a22d20571149fcaafc436b22b4aaf1983807
aleksl0l <aleksl0l@yandex.ru>
2018-08-16 00:08:00 +0300

    add delete

--- todo/models.go
+++ todo/models.go
@@ -1,8 +1,8 @@
 package todo
 
 import (
-	"fmt"
-	"todoapi/common"
+	"go-api-todo/common"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Todo struct {
@@ -20,6 +20,12 @@ func GetAllTodo() ([]Todo, error) {
 	db := common.GetDb()
 	var res []Todo
 	err := db.C("todo").Find(nil).All(&res)
-	fmt.Println(res)
+	//fmt.Println(res)
 	return res, err
+}
+
+func DeleteTodo(name string) error {
+	db := common.GetDb()
+	err := db.C("todo").Remove(bson.M{"name": name})
+	return err
 }
--- todo/router.go
+++ todo/router.go
@@ -1,8 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func TodoRegister(router *gin.RouterGroup) {
@@ -25,5 +27,21 @@ func TodoAdd(c *gin.Context) {
 }
 
 func TodoDelete(c *gin.Context) {
+	size_body, error := strconv.Atoi(c.Request.Header.Get("Content-Length"))
+	if error != nil {
+		c.JSON(http.StatusBadRequest, "error in header Content-Length")
+		return
+	}
+	buf := make([]byte, size_body)
+	num, _ := c.Request.Body.Read(buf)
+	var todo = &Todo{}
+	error = json.Unmarshal(buf[0:num], todo)
 
+	error = DeleteTodo(todo.Name)
+	//if error != nil {
+	//	c.JSON(http.StatusNotFound, "can't delete todo with name")
+	//	return
+	//}
+	panic("blya")
+	c.JSON(http.StatusOK, "success")
 }
